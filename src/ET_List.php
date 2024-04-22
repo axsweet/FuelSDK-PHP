25348019051b6c3013d5fe64ecd02e9909deb93d
alexander <alexander@uniquephoto.com>
2024-04-22 13:33:59 -0400

    Missing a declartion throwing a error

--- src/ET_List.php
+++ src/ET_List.php
@@ -13,8 +13,9 @@ class ET_List extends ET_CUDWithUpsertSupport
     * @var int 		Gets or sets the folder identifier.
     */
 	public  $folderId;
-
-	/** 
+    public $folderProperty;
+    public $folderMediaType;
+	/**
 	* Initializes a new instance of the class and set the property obj, folderProperty and folderMediaType to appropriate values.
 	*/		
 	function __construct()
